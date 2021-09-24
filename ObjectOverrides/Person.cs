484c79189dc2f30f6f01825bcb1455c26d0f2c65
Tigris <tabula.tenebris@gmail.com>
2021-09-24 20:13:47 +0600

    Add project files.

---
+++ ObjectOverrides/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+        public Person() { }
+        public override string ToString()=>
+            $"[{base.ToString()}; First Name: {FirstName}; Last Name: {LastName}; Age: {Age}]";
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj is Person&&obj!=null)
+        //    {
+        //        Person temp;
+        //        temp = (Person)obj;
+        //        if(FirstName==temp.FirstName&&LastName==temp.LastName&&Age==temp.Age)
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return false;
+        //}
+        public override bool Equals(object obj) => obj?.ToString() == ToString();
+    }
+}
---
+++ ObjectOverrides/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Jonah", "Jameson", 40);
+
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+
+            Person p2 = p1;
+            object o = p2;
+            if (o.Equals(p1)&&p2.Equals(o))
+                Console.WriteLine("Same instance!");
+            Console.WriteLine();
+
+            Person pers = new Person("Joseph", "Barkley", 32);
+            Console.WriteLine(pers.Equals(p1));
+        }
+    }
+}
